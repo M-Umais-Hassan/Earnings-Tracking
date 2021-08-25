2c7797715bf99076da8a2aff5fef2c6f8013412b
M-Umais-Hassan <umaishassan66u@gmail.com>
2021-08-26 00:03:25 +0500

    slight changes in table

--- src/Components/Admin/Analytics/Table.jsx
+++ src/Components/Admin/Analytics/Table.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './table.style.css';
 
 const Table = ({ allUsers }) => {
+    console.log()
     return (
         <div className="table__scroll">
             <table>
@@ -10,8 +11,6 @@ const Table = ({ allUsers }) => {
                     <tr>
                         <th><span>Name</span></th>
                         <th>Email</th>
-                        <th>Projects</th>
-                        <th>Earnings</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -21,8 +20,6 @@ const Table = ({ allUsers }) => {
                             <tr key={user.id}>
                                 <td><Link to={`/detail/${user.id}`}>{user.name && user.name}</Link></td>
                                 <td>{user.email && user.email}</td>
-                                <td>{user.projects ? user.projects.length : 0}</td>
-                                <td>{user.earnings ? user.earnings : 0}</td>
                             </tr> : null
                         )
                     })}
